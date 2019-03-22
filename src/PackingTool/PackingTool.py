c437388c118742e366d13d5d88e4d042d3501547
josonking <wangsanmu5@163.com>
2019-03-22 20:19:12 +0800

    up py

--- src/PackingTool/PackingTool.py
+++ src/PackingTool/PackingTool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 3.x
+#!/usr/bin/env python 3.6
 # encoding: utf-8
 
 import os.path
@@ -23,7 +23,7 @@ pathTree.create_node("/", "id")
 addFileTree = Tree()
 addFileTree.create_node("/", "id")
 
-actionTip = "\n+------------------------------------------------------------------------------------------------------------------------------------------------------------------------+\n| add: 添加 | rm: 移除 | addall: 添加所有 | rmall:移除所有 | lsm:查看所有模块 | lss:查看已选模块 | lsa: 查看所有模块及已选模块 | done: 完成 | exit:取消 | help: 查看指令 |\n+------------------------------------------------------------------------------------------------------------------------------------------------------------------------+"
+actionTip = "\n+-------------------------------------------------------------------+\n| add: 添加 | rm: 移除 | addall: 添加所有 | rmall:移除所有 |\n|+----------------------------------------------------------------+\nlsm: 查看所有模块 | lss: 查看已选模块 | lsa: 查看所有模块及已选模块 |\n+---------------------------------------------------------+\n|  done: 完成   |  exit: 取消   |   help: 查看指令   |\n+---------------------------------------------------------+"
 actionResponse = "[请选择操作]$ "
 
 # 遍历指定目录下所有文件，生成文件目录 pathTree
@@ -38,7 +38,6 @@ def handlePathFile(sourcePath, parentKey = 'id'):
   for index, dir in enumerate(fileDir):
     child = os.path.join('%s\%s' % (sourcePath, dir))
     if os.path.isfile(child):
-      print(len(os.listdir(sourcePath)))
       key = parentKey + (str(index) if index > 9 else '0' + str(index))
       pathTree.create_node(dir + '  ' + key, key, parentKey)
 
@@ -80,7 +79,6 @@ def rmAll(sourcePath):
 # 添加树节点
 def addNode(key):
   node = pathTree.get_node(key)
-  print(Node.is_leaf(node))
   name = pathTree.get_node(key).tag
   if len(key) == 4:
     addFileTree.create_node(name, key, "id")
@@ -96,22 +94,33 @@ def addNode(key):
 # param outFullName: 压缩文件保存路径+xxxx.zip
 # return: 无
 def zipDir(dirPath, outFullName):
-    zip = zipfile.ZipFile(outFullName,"w",zipfile.ZIP_DEFLATED)
-    for path,dirnames,filenames in os.walk(dirPath):
-        # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
-        fpath = path.replace(dirPath,'')
+  zip = zipfile.ZipFile(outFullName,"w",zipfile.ZIP_DEFLATED)
+  for path,dirnames,filenames in os.walk(dirPath):
+    # 去掉目标跟路径，只对目标文件夹下边的文件及文件夹进行压缩
+    fpath = path.replace(dirPath,'')
 
-        for filename in filenames:
-            zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
-    zip.close()
+    for filename in filenames:
+      zip.write(os.path.join(path,filename),os.path.join(fpath,filename))
+  zip.close()
 
-# 选择打包存放路径
+# 打包
 def pack():
   root = tk.Tk()
   root.withdraw()
   
-  packPath = filedialog.askdirectory()
-  print(packPath)
+  dialogPath = filedialog.askdirectory()
+  if not dialogPath.strip():
+    print('您已取消打包')
+    return 'cancleDialog'
+
+  packPath = os.path.join(dialogPath, 'package')
+  zipPath = os.path.join(dialogPath, 'zip')
+  if os.path.exists(packPath):
+    shutil.rmtree(packPath)
+    os.makedirs(packPath) 
+  if os.path.exists(zipPath):
+    shutil.rmtree(zipPath)
+  # print(packPath)
   print('打包中...')
 
   leafPathList = Tree.paths_to_leaves(addFileTree)
@@ -125,8 +134,8 @@ def pack():
         curLeafPath = os.path.join(curLeafPath, pathName)
         targetLeafPath  = os.path.join(targetLeafPath, pathName)
 
-    print(curLeafPath)
-    print(targetLeafPath)
+    # print(curLeafPath)
+    # print(targetLeafPath)
     targetLeafPath = targetLeafPath.replace('/', '\\')
     if curLeafPath.find('.') == -1:
       shutil.copytree(curLeafPath, targetLeafPath)
@@ -140,10 +149,12 @@ def pack():
 
   print('\n打包完成，开始压缩文件...')
   
-  outZipPath = os.path.join(packPath, 'dist.zip')
+  os.makedirs(zipPath)
+  outZipPath = os.path.join(zipPath, 'dist.zip')
   zipDir(packPath, outZipPath)
       
-  print('\n打包完成，模块输出路径：' + packPath + '/ dist.zip')
+  print('\n打包完成，模块输出路径：' + zipPath + '\dist.zip')
+  return 'success'
 
 # 根据指令处理文件
 def handleAction(sourcePath, action):
@@ -191,11 +202,19 @@ def handleAction(sourcePath, action):
       pathTree.show()
       print("\n已选模块：")
       addFileTree.show()
-      action = input(actionResponse)
+      
+      
     elif action == 'done':
       print('=> 选择模块完毕，请选择打包文件保存路径\n')
-      pack()
-      break
+      packResponse = pack()
+      if packResponse == 'cancleDialog':
+        action = input(actionResponse)
+      elif packResponse == 'success':
+        break
+      else:
+        print("打包失败，请重新操作\n")
+        print(actionTip)
+        action = input(actionResponse)
     elif action == 'exit':
       print('=> 您已取消打包并退出\n')
       break
@@ -207,36 +226,15 @@ def handleAction(sourcePath, action):
       print(actionTip)
       action = input(actionResponse)
 
-# 遍历指定目录，找出对应文件或者文件夹
-def handleFile(sourcePath, allowFile = True):
-  print('handleFile:' + sourcePath)
-  fileDir = os.listdir(sourcePath)
-  filesArray = []
-  for dir in fileDir:
-    child = os.path.join('%s\%s' % (sourcePath, dir))
-    if os.path.isfile(child) and allowFile:
-      print(dir)
-      print(child)
-      filesArray.append(dir)
-
-    if os.path.isdir(child) and len(os.listdir(child)) > 0:
-      print(dir)
-      print(child)
-      filesArray.append(dir)
-
-      childfilesArray = handleFile(child, False)  
-      if len(childfilesArray) > 0:
-        filesArray.append(childfilesArray)
-
-  return filesArray
-
 if __name__ == "__main__":
   # filePath = 'D:\\dev\\github\\antd-admin\\src\\components'  # refer root dir
   # E:\\code\\github\\antd-admin\\src\\components
 
   # 输入路径
-  # filePath = input("请输入文件路径：")
-  filePath = 'E:\\code\\github\\antd-admin\\src\\components'
+  # filePath = 'E:\\code\\github\\antd-admin\\src\\components'
+  print("+----------------------+\n| 欢迎使用模块打包工具 |\n+----------------------+\n")
+  filePath = input("请输入源文件路径：")
+
   while True:
     if os.path.exists(filePath):
       break
@@ -246,12 +244,9 @@ if __name__ == "__main__":
   # # 查看模块  
   # filesTree(filePath)
   handlePathFile(filePath)
-  print("+----------------------+\n| 欢迎使用模块打包工具 |\n+----------------------+\n")
   print("所有模块")
   pathTree.show()
 
   # 处理模块
   action = input(actionTip + "\n" + actionResponse)
   handleAction(filePath, action)
-
-
---
+++ src/PackingTool/pack.bat
@@ -0,0 +1,5 @@
+ECHO OFF
+SET PATH=%PATH%
+pip install -r requirements.txt
+python PackingTool.py
+PAUSE
